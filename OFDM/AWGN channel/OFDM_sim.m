652dac5ba0b007387fef1650ebc63579740c0cce
Sam Fang <asamjerry@gmail.com>
2022-03-22 21:49:11 +0800

    OFDM

--- OFDM/AWGN channel/OFDM_sim.m
+++ OFDM/AWGN channel/OFDM_sim.m
@@ -1,52 +1,61 @@
 %%  Assumption on data
 clear all
-bits_num=128*10^4;    % # of bits
+bits_num=10^5;    % # of bits
 fft_size=64;          % fft size
-input=rand(1,bits_num);     
-X(input>0.5)=1;       % X:Input bits
-X(input<=0.5)=-1;
+input=rand(64,bits_num)>0.5;     
+X=input*2-1;       % X:Input bits
+
 
 %% Channel assumption
-h=[1 zeros(1,fft_size-1)];      % AWGN channel impulse response
-Q=eye(fft_size);    % Time domain convolution matrix
+h=[1];      % AWGN channel impulse response
 H=fft(h,fft_size);   % AWGN channel frequency response
 H_matrix=diag(H);    % Frequency domain matrix  
 
 %% SNR
 OFDM_symbol_power=1;
 SNR=1:25;                % SNR = 1/(2*noise power on each part)
-noise_pow=1./(2*SNR);  % noise power = 1/(2*SNR) 
-BER=zeros(1,length(SNR));
+noise_pow=1./(SNR);  % noise power = 1/(2*SNR)
+BER = zeros(fft_size,length(SNR));
 %%
 for j=1:length(SNR)
-    error_num=0;
-    for i=0:fft_size:length(X)-fft_size
+    error_num=zeros(fft_size,1);
+    for i=1:bits_num
         %% TX
-        x=ifft(X(i+1:i+fft_size),fft_size)*sqrt(fft_size);     % x = ifft(X)
+        x=ifft(X(:,i),fft_size)*sqrt(fft_size);     % x = ifft(X)
         %% Channel, y: Received signal, Q: Convolution matrix
-        y=Q*transpose(x)+sqrt(noise_pow(j))*randn(fft_size,1)+sqrt(-1)*sqrt(noise_pow(j))*randn(fft_size,1); 
-        % y = Q*x + w, w is complex gaussian random vector
+        y=conv(h,x)+sqrt(noise_pow(j))*randn(fft_size,1)+sqrt(-1)*sqrt(noise_pow(j))*randn(fft_size,1); 
+        
         %% RX
         Y=fft(y,fft_size)/sqrt(fft_size);     % Y = fft(y)
         X2=inv(H_matrix)*Y;      % X2=Y/H (Equalization)
-        X2=transpose(X2);
         %% Detection
         Output(X2>=0)=1;
         Output(X2<0)=-1;
-        error_num=error_num+sum(Output~=X(i+1:i+fft_size));
+        error_num = error_num + (transpose(Output)~=X(:,i));
     end
-    BER(j)=error_num/bits_num;
+    BER(:,j)=error_num/bits_num;
 end
+average_BER=mean(BER);
 %%
 figure(1)
-theoretical_BER=qfunc(sqrt(2*SNR));  % Theoretical BER
-semilogy(10*log10(SNR),BER,'-o');
-hold on
+theoretical_BER=qfunc(sqrt(SNR));  % Theoretical BER
 semilogy(10*log10(SNR),theoretical_BER,'-*');
-legend('Simulation BER','Theoretical BER (Q[sqrt(2*SNR)]')
+hold on
+semilogy(10*log10(SNR),average_BER,'-o');
+legend('Theoretical BER (Q[sqrt(SNR)]','Average BER')
 title('BER curve');
 xlabel('SNR(dB)');ylabel('BER');
 grid minor
 grid on
-ylim([10^-5 1]);
-OFDM_symbol_power=mean(conj(transpose(x))*x)/fft_size;   % Power = E[||x||^2] 
+ylim([10^-4 1]);
+figure(2)
+semilogy(10*log10(SNR),theoretical_BER,'-*');
+hold on
+semilogy(10*log10(SNR),BER);
+legend('Theoretical BER (Q[sqrt(SNR)]','BER of every subcarriers')
+title('BER curve of every subcarriers');
+xlabel('SNR(dB)');ylabel('BER');
+grid minor
+grid on
+ylim([10^-4 1]);
+
