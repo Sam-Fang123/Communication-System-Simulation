6bc9bc213bc61513e71a69800602064f200e820b
Sam Fang <asamjerry@gmail.com>
2022-03-28 12:50:12 +0800

    Add word and ppt

--- Detection & Estiamtion/HW1.m
+++ Detection & Estiamtion/HW1.m
@@ -2,11 +2,17 @@
 clear all
 
 w=0:0.01:1;
-L=factorial(10)*(w.^7).*(1-w).^3/(factorial(7)*factorial(3));
+L=factorial(10)*(w.^6).*(1-w).^4/(factorial(6)*factorial(4));
 figure(1)
 plot(w,L)
+xticks([0:0.1:1])
+xlabel('Parameter w')
+ylabel('Likelihood L(w|n=10,y=6)')
+grid on
 
 figure(2)
 y=0:10;
-f=factorial(10)*(0.2.^y).*(0.8.^(10-y))./factorial(y)./factorial(10-y);
-stem(y,f)
+f=factorial(10)*(0.6.^y).*(0.4.^(10-y))./factorial(y)./factorial(10-y);
+bar(y,f);
+xlabel('Data y')
+ylabel('f(y|n=10,w=0.6)')
