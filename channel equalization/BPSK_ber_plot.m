16ed08914f5dfab6f73c171da5d40ad96b6561eb
Sam-Fang123 <asamjerry@gmail.com>
2022-07-04 16:11:32 +0800

    add zero forcing

--- channel equalization/BPSK_ber_plot.m
+++ channel equalization/BPSK_ber_plot.m
@@ -2,14 +2,15 @@
 SNR_db=1:14;
 %semilogy(SNR_db,BER_bpsk,'-o');
 %hold on
-semilogy(SNR_db,BER_wiener,'-+');
+semilogy(SNR_db,BER_wiener,'-+','LineWidth',1.5);
 ylabel('Bit error rate')
 xlabel('SNR(dB)')
 title('Error performance using different equalization scheme')
 grid on
 hold on
+semilogy(SNR_db,BER_ZF,'-o');
 semilogy(SNR_db,BER(1,:)/10^7,'-*');
 semilogy(SNR_db,BER(2,:)/10^7,'-x');
 semilogy(SNR_db,BER(3,:)/10^7,'-d');
 %legend('No channel equalization','Wiener filter','LMS,stepsize=0.075','LMS,stepsize=0.025','LMS,stepsize=0.0075');
-legend('Wiener filter','LMS,stepsize=0.075','LMS,stepsize=0.025','LMS,stepsize=0.0075');
+legend('Wiener filter','Zero Forcing','LMS,stepsize=0.075','LMS,stepsize=0.025','LMS,stepsize=0.0075');
