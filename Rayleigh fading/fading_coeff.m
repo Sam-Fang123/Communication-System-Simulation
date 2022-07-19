59b87cd9f54c84313a0df8c6696dbbffb205e242
Sam Fang <asamjerry@gmail.com>
2022-07-19 23:54:48 +0800

    debug

--- Rayleigh fading/fading_coeff.m
+++ Rayleigh fading/fading_coeff.m
@@ -1,12 +1,12 @@
 clear
 clc
-bitnum = 10^6;
+bitnum = 2*10^5;
 t=1:bitnum;
 
 %Flat Fading Channel
-velocity = 120;      % mobile velocity in km/hr
+velocity = 30;      % mobile velocity in km/hr
 Fc = 900;           % carrier freq in MHz
-Fs = 10^-3;      % sampling freq MHZ 
+Fs = 0.27;      % sampling freq MHZ 
                     % (we have 4 samples per pn chip, thus 1.2288*4)
 
 t=t/(Fs*10^6)*10^3;     % ms       
@@ -19,13 +19,12 @@ rng('default');
 inphase = 2*pi*rand(1, N0+1);
 
 [temp_re temp_im fade_coeff,inphase] = spfade(velocity,Fc,Fs,N0,bitnum,inphase);
-%db_re=10*log10(rms(temp_re,1));
-%db_im=10*log10(rms(temp_im,1));
 figure(1)
 plot(t,20*log10(abs(fade_coeff)));
-title('Rayleigh fading channel when vehicle velocity = 120km/h)')
+title('Rayleigh fading channel when vehicle velocity = 30km/h)')
 xlabel('time(ms)')
 ylabel('Signal Level(dB about rms)')
+xlim([0 740])
 grid on
 %ylabel('Attenuation gain |h|')
 
