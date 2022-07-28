31d826405d30483b008e02ce76732aab7c8f7a2b
Sam Fang <asamjerry@gmail.com>
2022-07-28 12:52:41 +0800

    c

--- Rayleigh fading/fading_coeff.m
+++ Rayleigh fading/fading_coeff.m
@@ -1,6 +1,6 @@
 clear
 clc
-bitnum = 2*10^5;
+bitnum = 10^6;
 t=1:bitnum;
 
 %Flat Fading Channel
@@ -18,10 +18,15 @@ inphase = zeros(1, N0+1);
 rng('default');
 inphase = 2*pi*rand(1, N0+1);
 
-[temp_re temp_im fade_coeff,inphase] = spfade(velocity,Fc,Fs,N0,bitnum,inphase);
+for i=1:bitnum:bitnum*100
+    [temp_re temp_im fade_coeff,inphase] = spfade(velocity,Fc,Fs,N0,bitnum,inphase);
+    fade_coeff_out(i:bitnum+i-1)=fade_coeff;
+end
+
+
 figure(1)
 plot(t,20*log10(abs(fade_coeff)));
-title('Rayleigh fading channel when vehicle velocity = 30km/h)')
+title('Rayleigh fading channel when velocity = 30km/h')
 xlabel('time(ms)')
 ylabel('Signal Level(dB about rms)')
 xlim([0 740])
@@ -31,28 +36,28 @@ grid on
 x1 = -2.5:0.01:2.5;
 x2 = 0:0.01:3;
 figure(2)
-histogram(abs(fade_coeff),'Normalization','pdf');
+histogram(abs(fade_coeff_out),'Normalization','pdf');
 ylabel('Probability Density');
 xlabel('X')
-title('PDF of X')
+title('PDF of X when velocity = 30km/h')
 hold on
 plot( x2, x2/(1/2).*exp(-x2.^2/2/(1/2)),'b-','LineWidth',2);
 hold off
 
 figure(3)
-histogram(temp_re,'Normalization','pdf');
+histogram(real(fade_coeff_out),'Normalization','pdf');
 ylabel('Probability Density');
 xlabel('Xi')
-title('PDF of Xi')
+title('PDF of Xi when velocity = 30km/h')
 hold on
 plot( x1, 1/sqrt(2*pi*(1/2))*exp(-x1.^2/2/(1/2)),'b-','LineWidth',2);
 hold off
 
 figure(4)
-histogram(temp_im,'Normalization','pdf');
+histogram(imag(fade_coeff_out),'Normalization','pdf');
 ylabel('Probability Density');
 xlabel('Xq')
-title('PDF of Xq')
+title('PDF of Xq when velocity = 30km/h')
 hold on
 plot(x1, 1/sqrt(2*pi*(1/2))*exp(-x1.^2/2/(1/2)),'b-','LineWidth',2);
 hold off
@@ -61,5 +66,5 @@ hold off
 %im_mean=mean(temp_im)
 %im_power=mean(temp_im.^2)
 
-channel_power=mean(temp_re.^2+temp_im.^2)
-channel_correlation=mean(temp_re.*temp_im)
+%channel_power=mean(temp_re.^2+temp_im.^2)
+%channel_correlation=mean(temp_re.*temp_im)
