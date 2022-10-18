9779b18ff2035bfd2321192bff9f60a9b4b8d92a
Sam Fang <asamjerry@gmail.com>
2022-10-19 01:00:56 +0800

    d

--- Serial equalization/Tang_ODM_window.m
+++ Serial equalization/Tang_ODM_window.m
@@ -1,6 +1,12 @@
 
 function [w]=Iter_SC_window(sys_par,rx_par,fade_struct,snr,Q)
 
-F = dftmtx(sys_par.tblock)/sqrt(sys_par.tblock);
-
-
+N = sys_par.tblock;
+F = dftmtx(N)/sqrt(N);
+B_N = [F(:,Q/2+1) F(:,Q/2)];
+Rho_BN = eye(N)-B_N*pinv(B_N);
+for m = 0:N-1
+    for n = 0:N-1
+        R_HH(m,n) = besselj(0,2*pi*fade_struct.nor_fd*(m-n));
+    end
+end
