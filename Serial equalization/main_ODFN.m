26b1d88011156587ae3bcb2b7bdcf44ddd134687
Sam Fang <asamjerry@gmail.com>
2022-11-02 13:13:40 +0800

    d

--- Serial equalization/main_ODFN.m
+++ Serial equalization/main_ODFN.m
@@ -46,7 +46,7 @@ tx_par.mod_nbits_per_sym = [1 2 4 6]; % bit of mod type
 tx_par.nbits_per_sym = tx_par.mod_nbits_per_sym(tx_par.mod_type);
 tx_par.pts_mod_const=2^(tx_par.nbits_per_sym); % points in modulation constellation
 
-tx_par.nblock= 10; % Number of transmitted blocks
+tx_par.nblock= 10000; % Number of transmitted blocks
 
 %% Rx parameter 钡Μ狠把计
 
@@ -68,7 +68,7 @@ rx_par.SE.K = [1 5 11 25];
 rx_par.IBDFE.cor_type_str={'GA cor','EST cor td', 'EST cor fd', 'TI cor_noth', 'TI cor_th'};% correlation coefficient
 rx_par.IBDFE.cor_type = 3;
 rx_par.IBDFE.eta = 1;%For and Correlation Estimator using TS(type 2) and type 3
-rx_par.IBDFE.D_type = [0 2 8 10];%For IBDFE T3C1 and T2C1_Quasibanded
+rx_par.IBDFE.D_type = [12];%For IBDFE T3C1 and T2C1_Quasibanded
 rx_par.IBDFE.first_iteration_full = 1;%For IBDFE T1C1, T3C1 ==> 1: use full block MMSE for first iteration
 %Parameter for iterative equalizer;
 rx_par.iteration = 4;
@@ -77,8 +77,8 @@ pilot.position = 0;
 
 %% Window 把计
 window_par.type_str={'no_window','Tang_window_ODM'};
-window_par.type = 1;
-window_par.Q = 4;
+window_par.type = 2;
+window_par.Q =rx_par.IBDFE.D_type*2 ;
 window_par.banded_str = {'Banded','Not banded'};
 window_par.banded = 1;
 
@@ -114,7 +114,7 @@ filename = filename + "_fd=" + num2str(fade_struct.fd);
 filename = filename + "_Nblock=" + num2str(tx_par.nblock);
 filename = filename + "_snr=" + snr.type_str(snr.type);
 filename = filename + "_window=" + window_par.type_str(window_par.type);
-%filename = filename + "_Q=" + num2str(window_par.Q);
+filename = filename + "_Q=" + num2str(window_par.Q);
 filename = filename + "_" + window_par.banded_str(window_par.banded);
 filename = filename + ".mat";
 filename
