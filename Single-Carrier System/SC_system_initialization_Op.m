e199dcd9c3f4b508db507516bbf4ed84b209d6aa
Sam-Fang123 <asamjerry@gmail.com>
2023-02-15 17:08:08 +0800

    d

--- Single-Carrier System/SC_system_initialization_Op.m
+++ Single-Carrier System/SC_system_initialization_Op.m
@@ -15,17 +15,21 @@ function [pilot,data,observation,contaminating_data,w,U,A] = SC_system_initializ
     end
     U = diag(w)*U; %let the basis matrix absorb the-time domain window
     U = orth(U);
+    power_allocation = 1/(1+sqrt((sys_par.L+1)/(sys_par.ndata/sys_par.G)));
+    pilot.power = sys_par.tblock*(1-power_allocation)/sys_par.G;
+    data.power = sys_par.tblock*power_allocation/sys_par.ndata;
     
     %Calculate the starting index and length of each cluster(Pg,Dg,Og)
     %Then the positions are also calculated
-    subblock_length = floor(sys_par.ndata/sys_par.G);
-    pilot.start_index = (1:sys_par.G)*subblock_length + 1;
+    subblock_length = floor(sys_par.tblock/sys_par.G);
+    pilot.start_index = (1:sys_par.G)*subblock_length-sys_par.P;
+    
     observation.start_index = pilot.start_index + est_par.l;
     pilot.start_index = mod(pilot.start_index-1,sys_par.tblock)+1;
     observation.start_index = mod(observation.start_index-1,sys_par.tblock)+1;
     
     pilot.cluster_length = sys_par.P+1;
-    observation.cluster_length = sys_par.P - 2*est_par.l+1;
+    observation.cluster_length = sys_par.P - 2*est_par.l + 1;
     contaminating_data.oneside_length = sys_par.M - 1 - est_par.l;
     
     pilot.position = zeros(sys_par.G,pilot.cluster_length); %each row contains indices of pilots corresponds to gth cluster
@@ -41,23 +45,17 @@ function [pilot,data,observation,contaminating_data,w,U,A] = SC_system_initializ
     pilot.position = mod(pilot.position-1,sys_par.tblock)+1;
     observation.position = mod(observation.position-1,sys_par.tblock)+1;
     contaminating_data.position = mod(contaminating_data.position-1,sys_par.tblock)+1;
+    pilot.position = [reshape(pilot.position.',1,[]) subblock_length*sys_par.G+1:sys_par.tblock];
     data.position = setdiff(1:sys_par.tblock, reshape(pilot.position.',1,[]));
     
     %pilot generate
     rand('state',sys_par.pilot_random_seed);
     randn('state',sys_par.pilot_random_seed);
-     switch(sys_par.pilot_scheme)
-        case 1 %use random constellation points as pilot
-            [const_pn dec_pn bit_pn] = block_sym_mapping(sys_par.nts,tx_par);
-            pilot.clusters_symbol = reshape(const_pn,(sys_par.P+1),sys_par.G).';%gth row is the pilot symbol of gth cluster
-            pilot.clusters_dec = reshape(dec_pn,(sys_par.P+1),sys_par.G).';
-        case 2 %only the middle pilot is nonzero pilot, and the average energy is same as data symbol
-            nonzero_pilot_symbol_num = sys_par.G;        
-            [const_pn dec_pn bit_pn]=block_sym_mapping(nonzero_pilot_symbol_num,tx_par);% generate TS seq (all blocks have same TS) %ts_par=1
-            pilot.clusters_symbol = [zeros(sys_par.G,floor((sys_par.P+1)/2)) const_pn.' zeros(sys_par.G,floor((sys_par.P+1)/2))];%each row is a pilot cluster
-            pilot.clusters_symbol = pilot.clusters_symbol*sqrt(sys_par.P+1);
-            pilot.clusters_dec = [zeros(sys_par.G,floor((sys_par.P+1)/2)) dec_pn.' zeros(sys_par.G,floor((sys_par.P+1)/2))];%each row is a pilot cluster
-    end
+    %only the middle pilot is nonzero pilot, and the average energy is same as data symbol
+    [const_pn dec_pn bit_pn]=block_sym_mapping(1,tx_par);% generate TS seq (all blocks have same TS) %ts_par=1
+    pilot.clusters_symbol = [zeros(sys_par.G,floor((sys_par.P+1)/2)) const_pn*ones(sys_par.G,1)*sqrt(pilot.power) zeros(sys_par.G,floor((sys_par.P+1)/2))];%each row is a pilot cluster
+    pilot.clusters_dec = [zeros(sys_par.G,floor((sys_par.P+1)/2)) dec_pn*ones(sys_par.G,1) zeros(sys_par.G,floor((sys_par.P+1)/2))];%each row is a pilot cluster
+    % 改到這邊!!
     
     
     %Channel Estimation Model Construct...
