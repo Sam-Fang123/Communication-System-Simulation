07c7c7d15d83b618d82060850ab97cf9ce0209a8
Sam Fang <asamjerry@gmail.com>
2023-03-27 19:16:38 +0800

    d

--- Single-Carrier System/data/Banded/plot_ber.m
+++ Single-Carrier System/data/Banded/plot_ber.m
@@ -63,7 +63,7 @@ xlim([0 indv.range(end)]);
 ylim([10^-5 0.5]);
 xticks(indv.range)
 %}
-
+%{
 figure(2)
 semilogy(indv.range,dv.BER(end,:),'->');
 xlabel('SNR (dB)');
@@ -79,13 +79,26 @@ semilogy(indv.range,dv5.BER(end,:),'-o');
 %semilogy(indv.range,dv8.BER(end,:),'-x');
 %semilogy(indv.range,dv9.BER(end,:),'-x');
 %semilogy(indv.range,dv10.BER(end,:),'-+');
-title('Optimal placement with N=256, BW eff=82.4, fd=0.2, IBDFE-T3C1 vs IBDFE-T2C1 Banded')
+title('Optimal placement with N=256, BW eff=82.4, fd=0.2, IBDFE-T3C1 vs IBDFE-T2C1 Banded(Estimated channel)')
 %legend('D = 24','D = 48','D = 96','D = 128','full')
 %legend('D = 2(With Tang window Q = 2)','D = 4(With Tang window Q = 4)','full(Without window)')
 %legend('D=2(Tang) T2C1(Estimated)','D=3(Tang) T2C1(Estimated)','D=4(Tang) T2C1(Estimated)','T3C1 D=2 1st full(Estimated)','T2C1 (Estimated)','D=2(Tang) T2C1','D=3(Tang) T2C1','D=4(Tang) T2C1','T3C1 D=2 1st full','T2C1')
 %legend('D=2 T2C1(Estimated)','D=4 T2C1(Estimated)','T3C1 D=2 1st full(Estimated)','T2C1 (Estimated)','D=2 T2C1','D=4 T2C1','T3C1 D=2 1st full','T2C1')
-legend('D=1(Tang) T2C1','D=2(Tang) T2C1','D=3(Tang) T2C1','D=4(Tang) T2C1','T3C1 D=2 1st full','T2C1')
+legend('D=1(Tang) T2C1','D=2(Tang) T2C1','D=3(Tang) T2C1','D=4(Tang) T2C1','T3C1 D=2 1st full')
 xlim([0 indv.range(end)]);
 %ylim([10^-5 0.5]);
 xticks(indv.range)
+%}
 
+figure(2)
+semilogy(indv.range,dv.BER(end,:),'--d');
+xlabel('SNR (dB)');
+ylabel('average BER');
+grid on;
+hold on;
+semilogy(indv.range,dv2.BER(end,:),'-d');
+title('Optimal placement with N=256, BW eff=82.4, fd=0.2,IBDFE-T2C1 Banded')
+legend('D=1(Tang) T2C1(Estimated)','D=1(Tang) T2C1(Ideal)')
+xlim([0 indv.range(end)]);
+ylim([10^-5 0.5]);
+xticks(indv.range)
