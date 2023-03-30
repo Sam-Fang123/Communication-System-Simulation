13c56058066ab039fec1306f5aa15ff1be12a4df
Sam-Fang123 <asamjerry@gmail.com>
2023-03-30 17:45:56 +0800

    d

--- Single-Carrier System/data/Banded/plot_ber.m
+++ Single-Carrier System/data/Banded/plot_ber.m
@@ -65,28 +65,30 @@ xticks(indv.range)
 %}
 
 figure(2)
-semilogy(indv.range,dv.BER(end,:),'->');
+semilogy(indv.range,dv.BER(end,:),'--*');
 xlabel('SNR (dB)');
 ylabel('average BER');
 grid on;
 hold on;
-semilogy(indv.range,dv2.BER(end,:),'-d');
+semilogy(indv.range,dv2.BER(end,:),'--o');
 semilogy(indv.range,dv3.BER(end,:),'-*');
-semilogy(indv.range,dv4.BER(end,:),'-->');
-semilogy(indv.range,dv5.BER(end,:),'--d');
-semilogy(indv.range,dv6.BER(end,:),'--*');
-%semilogy(indv.range,dv7.BER(end,:),'-*');
-%semilogy(indv.range,dv8.BER(end,:),'-x');
-%semilogy(indv.range,dv9.BER(end,:),'-x');
-%semilogy(indv.range,dv10.BER(end,:),'-+');
-title('Optimal placement with N=256, BW eff=82.4, fd=0.2, IBDFE-T3C1 1st Banded')
+semilogy(indv.range,dv4.BER(end,:),'-o');
+%semilogy(indv.range,dv5.BER(end,:),'-');
+%semilogy(indv.range,dv6.BER(end,:),'->');
+%semilogy(indv.range,dv7.BER(end,:),'-d');
+%semilogy(indv.range,dv8.BER(end,:),'-*');
+%semilogy(indv.range,dv9.BER(end,:),'-o');
+%semilogy(indv.range,dv10.BER(end,:),'-');
+title('Optimal placement with N=256, BW eff=82.4, fd=0.2, IBDFE-T3C1 vs T2C1')
+legend('1st banded D=4(Tang) T3C1(Est)','All banded D=4(Tang) T2C1(Est)','1st banded D=4(Tang) T3C1(Ideal)','All banded D=4(Tang) T2C1(Ideal)')
 %title('Optimal placement with N=256, BW eff=82.4, fd=0.2, IBDFE-T3C1 vs IBDFE-T2C1 Banded')
 %legend('D = 24','D = 48','D = 96','D = 128','full')
 %legend('D = 2(With Tang window Q = 2)','D = 4(With Tang window Q = 4)','full(Without window)')
 %legend('D=2(Tang) T2C1(Estimated)','D=3(Tang) T2C1(Estimated)','D=4(Tang) T2C1(Estimated)','T3C1 D=2 1st full(Estimated)','T2C1 (Estimated)','D=2(Tang) T2C1','D=3(Tang) T2C1','D=4(Tang) T2C1','T3C1 D=2 1st full','T2C1')
 %legend('D=2 T2C1(Estimated)','D=4 T2C1(Estimated)','T3C1 D=2 1st full(Estimated)','T2C1 (Estimated)','D=2 T2C1','D=4 T2C1','T3C1 D=2 1st full','T2C1')
 %legend('D=1(Tang) T2C1','D=2(Tang) T2C1','D=3(Tang) T2C1','D=4(Tang) T2C1','T3C1 D=2 1st full')
-legend('1st banded D=2(Tang) T3C1(Estimated)','1st banded D=4(Tang) T3C1(Estimated)','1st full T3C1(Estimated)','1st banded D=2(Tang) T3C1(Ideal)','1st banded D=4(Tang) T3C1((Ideal))','1st full T3C1((Ideal))')
+%legend('1st banded D=1(Tang) T3C1(Ideal)','1st banded D=2(Tang) T3C1(Ideal)','1st banded D=3(Tang) T3C1(Ideal)','1st banded D=4(Tang) T3C1((Ideal))','1st full T3C1((Ideal))')
+%legend('1st banded D=1(Tang) T3C1(Est)','1st banded D=2(Tang) T3C1(Est)','1st banded D=3(Tang) T3C1(Est)','1st banded D=4(Tang) T3C1(Est)','1st full T3C1(Est)')
 xlim([0 indv.range(end)]);
 ylim([10^-5 0.5]);
 %ylim([10^-3 0.5])
