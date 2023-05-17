83a411c16156c7969f6fd04354466d4bde741393
Sam Fang <asamjerry@gmail.com>
2023-05-18 01:18:03 +0800

    d

--- Single-Carrier System/data/Banded/plot_ber.m
+++ Single-Carrier System/data/Banded/plot_ber.m
@@ -135,23 +135,24 @@ xticks(indv.range)
 
 
 figure(2)
-semilogy(indv.range,dv.BER(end,:),'--o');
+semilogy(indv.range,dv.BER(end,:),'-o');
 xlabel('SNR (dB)');
 ylabel('average BER');
 grid on;
 hold on;
-semilogy(indv.range,dv2.BER(end,:),'--d');
-semilogy(indv.range,dv3.BER(end,:),'--*');
-semilogy(indv.range,dv4.BER(end,:),'--');
-semilogy(indv.range,dv5.BER(end,:),'-o');
-semilogy(indv.range,dv6.BER(end,:),'-d');
-semilogy(indv.range,dv7.BER(end,:),'-*');
-semilogy(indv.range,dv8.BER(end,:),'-');
+semilogy(indv.range,dv2.BER(end,:),'-d');
+semilogy(indv.range,dv3.BER(end,:),'-*');
+semilogy(indv.range,dv4.BER_est(end,:),'->');
+semilogy(indv.range,dv5.BER_est(end,:),'-^');
+semilogy(indv.range,dv6.BER_est(end,:),'-+');
+semilogy(indv.range,dv7.BER(end,:),'-');
 %title('Optimal placement N=256, BW eff=82.4, fd=0.2, T3C1 vs T4C1 1st Banded D1=2(Tang), D2=2, Ideal')
 %title('Optimal placement N=256, BW eff=82.4, fd=0.2, T3C1 vs T4C1 1st Banded D1=2(Tang), D2=1')
-title('Optimal placement N=256, BW eff=89.4, fd=0.02, T3C1 vs T4C1 1st Banded D1=1(Tang), D2=0')
-legend('T4C1 D3=2 Est','T4C1 D3=4 Est','T4C1 D3=6 Est','T3C1 Est','T4C1 D3=2 Ideal','T4C1 D3=4 Ideal','T4C1 D3=6 Ideal','T3C1 Ideal')
+title('Optimal placement N=256, BW eff=82.4, fd=0.2, T4C1 1st Banded D1=2(Tang), D2=1, Est channel')
+legend('T4C1 D3=2','T4C1 D3=4','T4C1 D3=8','T4C1 D3=16','T4C1 D3=32','T4C1 D3=64','T3C1')
 xlim([0 indv.range(end)]);
-ylim([10^-7 1]);
+ylim([10^-5 1]);
 xticks(indv.range)
+
+
 
