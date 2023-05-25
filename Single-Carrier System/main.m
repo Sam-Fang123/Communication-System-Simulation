a84a65ba1d686c99a0baa86e21539fc85746b34f
Sam-Fang123 <asamjerry@gmail.com>
2023-05-25 17:13:40 +0800

    d

--- Single-Carrier System/main.m
+++ Single-Carrier System/main.m
@@ -14,7 +14,7 @@ DE_option.type = DE_option.estimation_on + DE_option.detection_on*2;
 %Type 3: Channel Estimation And Detection Both Working
 %% Time Domain Window parameter 办跌怠耾猧竟
 td_window.str = ["No-windowing","MBAE-SOE","Tang"];
-td_window.type = 1;
+td_window.type = 3;
 td_window.Q = 4;
 %% System parameters(Frame structure)
 sys_par.ts_type_str = {'Non-optiaml','Optiaml'};
@@ -96,7 +96,7 @@ tx_par.mod_nbits_per_sym = [1 2 4 6]; % bit of mod type
 tx_par.nbits_per_sym = tx_par.mod_nbits_per_sym(tx_par.mod_type);
 tx_par.pts_mod_const=2^(tx_par.nbits_per_sym); % points in modulation constellation
 
-tx_par.nblock= 100; % Number of transmitted blocks
+tx_par.nblock= 10000; % Number of transmitted blocks
 %% Train parameters 癡絤才じ把计
 ts_par.mod_type_str={'BPSK','QPSK','16QAM','64QAM'};
 ts_par.mod_type = 1; % 1: BPSK
@@ -138,7 +138,7 @@ rx_par.type_str={
     
     'IBDFE_T4C1';
     };
-rx_par.type = 9;
+rx_par.type = 11;
 
 %{
 Parameters for IBDFE ==> 
@@ -152,9 +152,9 @@ rx_par.IBDFE.cor_type_str={'GA cor','EST cor td', 'EST cor fd', 'TI cor_noth', '
 rx_par.IBDFE.cor_type = 3;
 rx_par.IBDFE.eta = 1;%For and Correlation Estimator using TS(type 2) and type 3
 rx_par.IBDFE.D = 1;%For IBDFE T3C1 and T2C1_Quasibanded
-rx_par.IBDFE.first_iteration_full = 1;%For IBDFE T1C1, T3C1==>1:use full block MMSE for first 2:use banded channel matrix(For T2C1, all iteration using banded)
+rx_par.IBDFE.first_iteration_full = 2;%For IBDFE T1C1, T3C1==>1:use full block MMSE for first 2:use banded channel matrix(For T2C1, all iteration using banded)
 rx_par.IBDFE.frist_banded_D = 2;
-rx_par.IBDFE.FB_D = 4;  % For IBDFE T4C1
+rx_par.IBDFE.FB_D = 3;  % For IBDFE T4C1
 td_window.Q = rx_par.IBDFE.frist_banded_D*2;
 %Parameter for iterative equalizer;
 rx_par.iteration = 4;
