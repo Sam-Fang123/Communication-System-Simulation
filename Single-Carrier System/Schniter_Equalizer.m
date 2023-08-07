2996754cb7f34d91efec8239c59f093f6dee6541
Sam Fang <asamjerry@gmail.com>
2023-08-07 12:24:15 +0800

    d

--- Single-Carrier System/Schniter_Equalizer.m
+++ Single-Carrier System/Schniter_Equalizer.m
@@ -17,14 +17,20 @@ C_beta = F*diag(w)*F';
 
 H_W = H_W.*rx_par.B_mtx;
 C_beta = C_beta.*rx_par.B_mtx;
+
 s_bar = zeros(sys_par.tblock,1);
-diag_v = eye(sys_par.tblock);
+I = eye(sys_par.tblock);
+s_bar(reshape(pilot.position.',1,[]))=reshape(pilot.clusters_symbol.',1,[]);
+s_var = ones(sys_par.tblock,1);
+s_var(reshape(pilot.position.',1,[]))=0;
+diag_v = diag(s_var);
+
 t_hat = zeros(sys_par.tblock,1);
 t_bar = F*s_bar;
 for ii=1:sys_par.tblock
     H_k = H_W(mod(ii-rx_par.IBDFE.frist_banded_Q-2+(1:2*rx_par.IBDFE.frist_banded_Q+1),sys_par.tblock)+1,:);
     C_k = C_beta(mod(ii-rx_par.IBDFE.frist_banded_Q-2+(1:2*rx_par.IBDFE.frist_banded_Q+1),sys_par.tblock)+1,:);
-    g_k = (H_k*H_k' + noise_pwr*(C_k*C_k'))\(H_k*diag_v(:,ii));
+    g_k = (H_k*H_k' + noise_pwr*(C_k*C_k'))\(H_k*I(:,ii));
     t_hat(ii) = t_bar(ii)+g_k'*(Y_W(mod(ii-rx_par.IBDFE.frist_banded_Q-2+(1:2*rx_par.IBDFE.frist_banded_Q+1),sys_par.tblock)+1)-H_k*t_bar);
 end
 1
