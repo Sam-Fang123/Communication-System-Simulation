0f51308d29b91f9322a72326f85347c060708ea9
Sam Fang <asamjerry@gmail.com>
2023-08-14 11:09:23 +0800

    d

---
+++ Single-Carrier System/Paper_fig/fd=0.1/QPSK/6_25_est_vs_ideal/plot_ber.m
@@ -0,0 +1,28 @@
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_IBDFE_TV_1st_banded_Q=2_D_FF=1_D_FB_full_QPSK_fd=0.1_N=256_I=5_M=5_Nblock=10000.mat')
+dv3=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_IBDFE_TV_1st_banded_Q=2_D_FF=1_D_FB=2_QPSK_fd=0.1_N=256_I=5_M=5_Nblock=10000.mat')
+dv2=dv;
+load('D&E-mode_indv=SNR_No-windowing_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_full_QPSK_fd=0.1_N=256_I=5_M=5_Nblock=10000.mat')
+figure(2)
+semilogy(indv.range,dv.BER_est(1,:),'-o');
+xlabel('SNR (dB)');
+ylabel('average BER');
+grid on;
+hold on;
+semilogy(indv.range,dv2.BER_est(1,:),'->');
+semilogy(indv.range,dv2.BER_est(3,:),'-s');
+semilogy(indv.range,dv3.BER_est(3,:),'-*');
+semilogy(indv.range,dv.BER_ideal(1,:),'-->');
+semilogy(indv.range,dv2.BER_ideal(1,:),'--o');
+semilogy(indv.range,dv2.BER_ideal(3,:),'--s');
+semilogy(indv.range,dv3.BER_ideal(3,:),'--*');
+title('QPSK BER (Normalized Doppler Frequency = 0.1), IBDFE-TV D_F_F=1')
+legend('Full-MMSE-LE, no windowing','Banded-MMSE-LE Q=2 [6]', ...
+    'IBDFE-TV D_F_B=1, 1st iteration use Banded-MMSE-LE Q=2 [6], 3th iteration',...
+    'IBDFE-TV D_F_B is full, 1st iteration use Banded-MMSE-LE Q=2 [6], 3th iteration',...
+    'Full-MMSE-LE, no windowing (Perfect CSI)','Banded-MMSE-LE Q=2 [6] (Perfect CSI)', ...
+    'IBDFE-TV D_F_B=1, 1st iteration use Banded-MMSE-LE Q=2 [6], 3th iteration (Perfect CSI)',...
+    'IBDFE-TV D_F_B is full, 1st iteration use Banded-MMSE-LE Q=2 [6], 3th iteration (Perfect CSI)')
+xlim([0 indv.range(end)]);
+ylim([10^-6 1]);
+xticks(indv.range)
---
+++ Single-Carrier System/Paper_fig/fd=0.5/Q=1_to_6_I=11_0.5/plot_Ber.m
@@ -0,0 +1,34 @@
+load('D&E-mode_indv=SNR_No-windowing_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_full_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+dv7=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_banded_Q=6_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+dv6=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_banded_Q=5_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+dv5=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_banded_Q=4_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+dv4=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_banded_Q=3_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+dv3=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_banded_Q=2_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+dv2=dv;
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_MMSE_FD_LE_banded_Q=1_QPSK_fd=0.5_N=256_I=11_M=5_Nblock=10000.mat')
+
+
+figure(2)
+semilogy(indv.range,dv.BER_est(end,:),'-d');
+xlabel('SNR (dB)');
+ylabel('BER');
+grid on;
+hold on;
+semilogy(indv.range,dv2.BER_est(end,:),'-o');
+semilogy(indv.range,dv3.BER_est(end,:),'->');
+semilogy(indv.range,dv4.BER_est(end,:),'-s');
+semilogy(indv.range,dv5.BER_est(end,:),'-x');
+semilogy(indv.range,dv6.BER_est(end,:),'-+');
+semilogy(indv.range,dv7.BER_est(end,:),'-');
+legend('Q=1','Q=2','Q=3','Q=4','Q=5','Q=6','Full')
+title('MMSE-FD-LE, fd=0.5, QPSK, Est CSI(I=11)')
+xlim([0 indv.range(end)]);
+ylim([10^-5 1]);
+xticks(indv.range)
+
+
