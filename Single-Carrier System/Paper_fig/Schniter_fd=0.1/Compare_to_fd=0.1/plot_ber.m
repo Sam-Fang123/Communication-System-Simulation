108633dc5f740eb5232bfbc111daf103f234a1cc
Sam Fang <asamjerry@gmail.com>
2023-08-19 02:17:09 +0800

    d

--- Single-Carrier System/Paper_fig/Schniter_fd=0.1/Compare_to_fd=0.1/plot_ber.m
+++ Single-Carrier System/Paper_fig/Schniter_fd=0.1/Compare_to_fd=0.1/plot_ber.m
@@ -1,7 +1,7 @@
 clear
 load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_IBDFE_TV_1st_banded_Q=2_D_FF=1_D_FB_full_BPSK_fd=0.1_N=256_I=5_M=5_Nblock=10000.mat')
 dv5=dv;
-load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_IBDFE_TV_1st_banded_Q=2_D_FF=1_D_FB=2_BPSK_fd=0.1_N=256_I=5_M=5_Nblock=10000.mat')
+load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_IBDFE_TV_1st_banded_Q=2_D_FF=1_D_FB=2_BPSK_fd=0.1_N=256_I=5_M=5_Nblock=50000.mat')
 dv4=dv;
 load('D&E-mode_indv=SNR_Tang_ZP_Optiaml_GCE-BEM_MMSE_Schniter_D=1_BPSK_fd=0.1_N=256_I=5_M=5_Nblock=10000.mat')
 dv3=dv;
